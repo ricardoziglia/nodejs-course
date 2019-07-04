cf77e544a40a75d33401df750b7950467a3ea947
ricardo.ziglia@osf-commerce.com <ricardo.ziglia@osf-commerce.com>
2019-07-04 17:39:29 -0300

    [nodejs-course] Added routes and html files.

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.use('/', (req, res, next) =>{
-    console.log('Generic middleware');
-    next(); // goes to next middleware in line (from top to botton).
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/add-product', (req, res, next) =>{
-    console.log('In add product middleware!');
-    res.send('<h1>Add product</h1>');
-});
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/', (req, res, next) =>{
-    console.log('In another middleware!');
-    res.send('<h1>Hello there!</h1>'); //adds some extra setup to the response (header and etc).
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
 });
 
 app.listen(3000);
