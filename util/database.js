56973102b21f21dd3ccf6f66c704148b6f17635f
ricardo.ziglia@osf-commerce.com <ricardo.ziglia@osf-commerce.com>
2019-07-16 15:11:27 -0300

    [nodejs-course] Added Sequelize module, small code fixes.

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+const sequelize = new Sequelize('node-complete', 'root', '@node.js', {
   dialect: 'mysql',
   host: 'localhost'
 });
