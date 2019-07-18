5edf87b5d238af7ad1b3923dd3c7fe81f7ceb65a
ricardo.ziglia@osf-commerce.com <ricardo.ziglia@osf-commerce.com>
2019-07-18 10:30:14 -0300

    [nodejs-course] Converted all the functionalities to use mongodb.

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
-
 const User = require('./models/user');
 
 const app = express();
@@ -22,8 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('5d2f883d0f79aa59fcf547bc')
     .then(user => {
-      req.user = user;
-      console.log(user);
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
