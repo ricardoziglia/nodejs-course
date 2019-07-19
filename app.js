70ba4560935f744cbe04a45b001408ae3165ca42
ricardo.ziglia@osf-commerce.com <ricardo.ziglia@osf-commerce.com>
2019-07-19 13:04:12 -0300

    [nodejs-course] Converted all the functionalities to use mongoose.

--- app.js
+++ app.js
@@ -36,14 +36,17 @@ app.use(
   })
 );
 
-// app.use((req, res, next) => {
-//   User.findById('5d30d70cc1d23c52b8c7fbfc')
-//     .then(user => {
-//       req.user = user;
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
